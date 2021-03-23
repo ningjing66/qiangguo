3e9a432e8ffdb05ebe9b5ef814236c8f52bc4fca
ningjing66 <syw99@163.com>
2021-03-23 10:38:43 +0800

    vue2021.03.23

---
+++ [startcmd - git - 副本 (5).cmd
@@ -0,0 +1,95 @@
+
+
+
+
+
+dddcopy "C:\Users\Administrator.Windwos 10YKMNH\.ssh\id_rsagitwork" "C:\Users\Administrator.Windwos 10YKMNH\.ssh\id_rsa"  /Y
+
+
+git init .
+git config --global user.name "shijiangdage"
+git config --global user.email  "81173510@qq.com"
+
+
+git init .
+git config --global user.name "ningjing66"
+git config --global user.email  "syw99@163.com"
+
+
+
+git remote add dev git@github.com:ningjing66/qiangguo.git
+git remote add dee git@github.com:ningjing66/qiangguo.git
+git remote add dvv https://github.com/ningjing66/qiangguo.git
+git status
+git remote -v
+git config --global --list
+git checkout -b master
+git branch
+ssh -T git@github.com
+
+git checkout -b main
+git add .
+git commit -m "vue"%date%
+git push dee main
+git push dvv main
+git push --force dee main
+
+
+
+@pause
+
+
+@pause
+
+exit
+
+exit
+
+
+git checkout -b master
+git branch
+ssh -T git@github.com
+git add .
+git commit -m "vue"%date%
+git status
+git remote -v
+git push --force dee master
+
+
+
+exit
+
+exit
+
+d:
+cd D:\tencentnodejs\git2020\bin
+
+d:
+cd D:\tencentnodejs\git2020\bin
+
+git branch 
+
+git checkout -b map-cover-view
+
+git add .
+
+git commit -m "map-cover-view"
+
+git push --all
+
+git branch
+
+
+@pause
+@echo off
+color a
+@start
+exit
+
+
+@rem echo on @cmd  @python aa1.py  @python aa01.py
+@python [starttestpython.py
+@rem echo on @cmd  @python aa1.py  @python aa11.py
+
+@rem powershell
+@start
